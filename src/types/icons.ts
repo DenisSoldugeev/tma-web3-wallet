b17fb13201f339185891fa04aaf95d15b605793f
Denis Soldugeev <den.soldugeev@yandex.ru>
2025-11-23 19:10:36 +0400

    feat: add Ton symbol icon and implement shadow variables for enhanced UI

--- src/types/icons.ts
+++ src/types/icons.ts
@@ -9,6 +9,7 @@ import ReceiveIcon from '@assets/icons/receive.svg?react';
 import SendIcon from '@assets/icons/send.svg?react';
 import SettingsIcon from '@assets/icons/settings.svg?react';
 import ShieldIcon from '@assets/icons/shield.svg?react';
+import TonSymbolIcon from '@assets/icons/ton-symbol.svg?react';
 import WalletIcon from '@assets/icons/wallet.svg?react';
 
 export const iconMap = {
@@ -24,6 +25,7 @@ export const iconMap = {
     eye: EyeIcon,
     eyeOff: EyeOffIcon,
     shield: ShieldIcon,
+    tonSymbol: TonSymbolIcon,
 } as const;
 
 export type IconName = keyof typeof iconMap;
